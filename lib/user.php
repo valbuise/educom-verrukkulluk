913563b17bbba4b7fd0b76e868cf7ebadeb9e3fc
valbuise <valbuise@gmail.com>
2022-02-23 15:41:49 +0100

    #6 user class vervaardigd

---
+++ lib/user.php
@@ -0,0 +1,23 @@
+<?php 
+
+class user {
+
+    private $connection;
+
+    public function __construct($connection){
+        $this->connection = $connection;
+    }
+
+    public function selecteerUser($user_id){
+
+        $sql="select * from user where id = $user_id";
+
+        $result = mysqli_query($this->connection, $sql);
+        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        return $user;
+    }
+}   
+
+
+?>
