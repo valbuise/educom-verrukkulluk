611d8cb3232aaf97dc3aca5a2ba101f9abc025d9
valbuise <valbuise@gmail.com>
2022-02-24 11:58:21 +0100

    #7 kitchentype class vervaardigd

---
+++ lib/kitchentype.php
@@ -0,0 +1,20 @@
+<?php 
+
+class kitchenType {
+    private $connection;
+
+    public function __construct($connection){
+        $this->connection = $connection;
+    }
+
+    public function selectKitchenType($keuken_type_id){
+
+        $sql = "select * from keuken_type where id = $keuken_type_id";
+
+        $result = mysqli_query($this->connection, $sql);
+        $keuken_type = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        return($keuken_type);
+
+    }
+}
