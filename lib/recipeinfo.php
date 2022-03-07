6ca11c987d832ba0f4d8b1e9080ed390dd5dae58
valbuise <valbuise@gmail.com>
2022-03-07 17:04:44 +0100

    #10 recipeclass nog geupdated!

--- lib/recipeinfo.php
+++ lib/recipeinfo.php
@@ -58,9 +58,9 @@ class recipeinfo{
 
     public function addFavorite($gerecht_id, $user_id){            
 
-        $this->deleteFavorite($gerecht_id, $user_id);// de deleteFavorite functie hier callen;
+        $this->deleteFavorite($gerecht_id, $user_id);// de deleteFavorite functie hier callen, zodat een user niet oneindig hetzelfde gerecht kan toevoegen als favoriet;
 
-        $sql = "insert into gerecht_info (record_type, gerecht_id, user_id) values ('F', '$gerecht_id', '$user_id')"; //hier ga ik een regel schrijven waar er een F wordt toegevoegd aan record_type, voor welk user_id dit is en welk gerecht_id;
+        $sql = "insert into gerecht_info (record_type, gerecht_id, user_id) values ('F', '$gerecht_id', '$user_id')"; // user (id) slaat gerecht (id) op als F(avoriet);
 
         $result = mysqli_query($this->connection, $sql);
 
@@ -70,7 +70,7 @@ class recipeinfo{
 
     public function deleteFavorite($gerecht_id, $user_id){
 
-        $sql = "delete from gerecht_info where record_type = 'F' and gerecht_id = $gerecht_id and user_id = $user_id";
+        $sql = "delete from gerecht_info where record_type = 'F' and gerecht_id = $gerecht_id and user_id = $user_id"; // user (id) verwijderd gerecht (id) als F(avoriet);
 
         $result = mysqli_query($this->connection, $sql);
         
