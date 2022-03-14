f439f7d24f3c234b1577c0211c433775d9ba4aa1
valbuise <valbuise@gmail.com>
2022-03-14 10:08:47 +0100

    #12

--- lib/shoppinglist.php
+++ lib/shoppinglist.php
@@ -26,17 +26,13 @@ class shoppinglist {
 
 
 
-
     public function addToList($gerecht_id, $user_id){
 
     $ingredients = $this->selectIngredient($gerecht_id);
 
     $total = 0;
 
-    foreach ($ingredients as $ingredient) {
-
-    if($this->artikelOnList($ingredient['artikel_id'], $user_id) == false) {
-
+    foreach ($ingredients as $ingredient) {  
             
     $artikel = $ingredient['artikel_id'];
     $nodig = $ingredient['aantal'] / $ingredient['verpakking'];
@@ -44,37 +40,16 @@ class shoppinglist {
     $aantal = $ingredient['aantal'];
     $prijs = $ingredient['prijs'];
 
+    if(!$this->artikelOnList($ingredient['artikel_id'], $user_id)) {
+
     $sql = "insert into boodschappenlijst(user_id, artikel_id, verpakking, aantal, prijs) values (1, $artikel, $verpakking, $aantal, $prijs)";
 
     $result = mysqli_query($this->connection, $sql);
 
-    
-
-    if ($ingredient['aantal'] > $ingredient['verpakking']) {
-
-        $artikel_id = $ingredient['artikel_id'];
-
-        $extra_nodig = $ingredient['aantal'] / $ingredient['verpakking'];
-
-        $extra_verpakking = ceil($extra_nodig);
-
-        $sql = "update boodschappenlijst set verpakking = $extra_verpakking where user_id = $user_id and artikel_id = $artikel_id";
-
-        $result = mysqli_query($this->connection, $sql);
-      
-    }
-        
+                
     } else {
 
-        if ($ingredient['aantal'] > $ingredient['verpakking']) {
-
-            $artikel_id = $ingredient['artikel_id'];
-
-            $extra_nodig = $ingredient['aantal'] / $ingredient['verpakking'];
-
-            $extra_verpakking = ceil($extra_nodig);
-
-            $sql = "update boodschappenlijst set verpakking = $extra_verpakking where user_id = $user_id and artikel_id = $artikel_id";
+            $sql = "update boodschappenlijst set verpakking = $verpakking where user_id = $user_id and artikel_id = $artikel";
 
             $result = mysqli_query($this->connection, $sql);
           
@@ -82,38 +57,26 @@ class shoppinglist {
 
     }
 
-    $total += $result;
-
-    
-    
-    }  
-
     $total ++;
 
     } 
 
-        
 
     public function artikelOnList($artikel_id, $user_id){
 
         $sql = "select * from boodschappenlijst where user_id = $user_id";
-
         $result = mysqli_query($this->connection, $sql);
-
         $groceries = mysqli_fetch_all($result, MYSQLI_ASSOC);                 
 
-        foreach($groceries as $grocerie){
+        foreach( $groceries as $grocerie ){
 
-            if ($grocerie['artikel_id']==$artikel_id){
+            if ( $grocerie['artikel_id'] == $artikel_id ){
 
                 return $grocerie;
 
             }
-        
-
         }
 
         return(false);
     }
-
 }
