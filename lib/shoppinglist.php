cc5ad6a866d5d6ea2e145830d5c3c9f56d29964c
valbuise <valbuise@gmail.com>
2022-04-04 16:02:26 +0200

    #15 boodschappenlijst class en pagina vervaardigd

--- lib/shoppinglist.php
+++ lib/shoppinglist.php
@@ -6,13 +6,15 @@ class shoppinglist {
     private $user;
     private $ingredient;
     private $grocerie;
+    private $artikel;
+    private $boodschap;
 
 
     public function __construct($connection) {
         $this->connection = $connection;
         $this->user = new user($connection);
         $this->ingredient = new ingredient($connection);
-
+        
     }
 
     private function selectUser($user_id){
@@ -25,44 +27,64 @@ class shoppinglist {
         return $ingredient;
     }
 
+    private function calcPrijs($user_id){
+
+        $sql = "select sum(prijs) as sum_prijs from boodschappenlijst where user_id = $user_id";
+
+        $result = mysqli_query($this->connection, $sql);
+
+        $row = mysqli_fetch_assoc($result); 
+
+        $totaalPrijs = $row['sum_prijs'];
+
+        $totaalPrijsInEuro = $totaalPrijs / 100;
+
+        return($totaalPrijsInEuro);
+    }
+      
+
     public function addToList($gerecht_id, $user_id){
 
     $ingredients = $this->selectIngredient($gerecht_id);
 
-    $total = 0;
-
     foreach ($ingredients as $ingredient) {  
-            
+    
+    $afbeelding = $ingredient['afbeelding'];
     $artikel = $ingredient['artikel_id'];
+    $titel = $ingredient['naam'];
+    $omschrijving = $ingredient['omschrijving'];
+    
+
     $nodig = $ingredient['aantal'] / $ingredient['verpakking'];
     $verpakking = ceil($nodig);
     $aantal = $ingredient['aantal'];
-    $prijs = $ingredient['prijs'];
+    $prijs = $ingredient['prijs'] * $verpakking;
+    $prijs_verpakking = $ingredient['prijs'];
 
-        if(!$this->artikelOnList($ingredient['artikel_id'], $user_id)) {
+    $opLijst = $this->artikelOnList($ingredient['artikel_id'], $user_id);
 
-        $sql = "insert into boodschappenlijst(user_id, artikel_id, verpakking, aantal, prijs) values (1, $artikel, $verpakking, $aantal, $prijs)";
+        if(!$opLijst) {
+
+        $sql = "insert into boodschappenlijst(user_id, artikel_id, titel, omschrijving, verpakkingen, aantal, prijs, prijsverpakking, afbeelding) values (1, '$artikel', '$titel', '$omschrijving', '$verpakking', '$aantal', '$prijs', '$prijs_verpakking', '$afbeelding')";
 
         $result = mysqli_query($this->connection, $sql);
 
                 
         } else {
         
-        $grocerie = $this->artikelOnList($ingredient['artikel_id'], 1);
-        $extra_ingredient = $ingredient['aantal'] + $grocerie['aantal'];
+        $extra_ingredient = $ingredient['aantal'] + $opLijst['aantal'];
         $extra_nodig = $extra_ingredient / $ingredient['verpakking'];
         $extra_verpakking = ceil($extra_nodig);
+        $extra_prijs = ($ingredient['prijs'] * $extra_verpakking) + $oplijst['prijs'];
+        
 
-        $sql = "update boodschappenlijst set aantal = $extra_ingredient, verpakking = $extra_verpakking where user_id = $user_id and artikel_id = $artikel";
+        $sql = "update boodschappenlijst set aantal = $extra_ingredient, verpakkingen = $extra_verpakking, prijs = $extra_prijs where user_id = $user_id and artikel_id = $artikel";
 
         $result = mysqli_query($this->connection, $sql);
           
         }
 
     }
-
-    $total ++;
-
     } 
 
 
@@ -83,4 +105,64 @@ class shoppinglist {
 
         return(false);
     }
+
+
+    
+    public function selectList($user_id){
+
+        $boodschappenlijst = [];
+        
+        $sql = "select * from boodschappenlijst where user_id = $user_id";
+            
+        $result = mysqli_query($this->connection, $sql);
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+
+            $totaalPrijs = $this->calcPrijs($row['user_id']);
+
+            $boodschappenlijst[] = [
+                "artikel_id" => $row['artikel_id'],
+                "titel" => $row['titel'],
+                "omschrijving" => $row['omschrijving'],
+                "verpakkingen" => $row['verpakkingen'],
+                "prijs" => $row['prijs'],
+                "prijsverpakking" => $row['prijsverpakking'],
+                "afbeelding" => $row['afbeelding'],
+                "totaalprijs" => $totaalPrijs,
+            ];
+
+        }
+            
+        return($boodschappenlijst);
+    
+    }
+    
+
+    public function deleteAll(){
+
+        $sql = "delete from boodschappenlijst";
+
+        $result = mysqli_query($this->connection, $sql);
+
+        return($result);
+    }
+
+    public function deleteArtikel($artikel_id){
+
+        $sql = "update boodschappenlijst set verpakkingen = verpakkingen - 1, prijs = prijs - prijsverpakking where artikel_id = $artikel_id";
+
+        $result = mysqli_query($this->connection, $sql);
+
+        return($result);
+    }
+
+    public function updateArtikel($artikel_id){
+
+        $sql = "update boodschappenlijst set verpakkingen = verpakkingen + 1, prijs = prijs + prijsverpakking where artikel_id = $artikel_id";
+
+        $result = mysqli_query($this->connection, $sql);
+
+        return($result);
+    }
+        
 }
